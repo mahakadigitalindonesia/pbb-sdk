74a6abdb9dc9f137b14d96c1617b94a6e450b07c
Abdul Aziz Nurfarhana <abaz.nurfarhana@gmail.com>
2021-10-22 10:44:26 +0700

    Refactor Code on KelurahanService

--- src/Services/KelurahanServiceImpl.php
+++ src/Services/KelurahanServiceImpl.php
@@ -7,13 +7,13 @@ namespace Mdigi\PBB\Services;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Mdigi\PBB\Contracts\KelurahanService;
 use Mdigi\PBB\Dtos\Blok as BlokDto;
+use Mdigi\PBB\Dtos\BlokZonaNilaiTanah as BlokZonaNilaiTanahDto;
 use Mdigi\PBB\Dtos\Kelurahan as KelurahanDto;
 use Mdigi\PBB\Helpers\OPColumns;
 use Mdigi\PBB\Models\Blok;
 use Mdigi\PBB\Models\BlokZonaNilaiTanah;
 use Mdigi\PBB\Models\Kecamatan;
 use Mdigi\PBB\Models\Kelurahan;
-use Mdigi\PBB\Dtos\BlokZonaNilaiTanah as BlokZonaNilaiTanahDto;
 
 class KelurahanServiceImpl implements KelurahanService
 {
@@ -52,14 +52,14 @@ class KelurahanServiceImpl implements KelurahanService
         ])->join(Kecamatan::table,
             Kecamatan::kodeKecamatan,
             '=',
-            Kelurahan::kodeKecamatan)->orderByRaw(Kelurahan::kodeKecamatan)->orderBy(Kelurahan::kodeKelurahan);
+            Kelurahan::kodeKecamatan)->orderBy(Kelurahan::kodeKecamatan)->orderBy(Kelurahan::kodeKelurahan);
     }
 
     public function blokZonaNilaiTanah($kodeKecamatan, $kodeKelurahan, $kodeBlok, $kode = null)
     {
         $data = BlokZonaNilaiTanah::query()->where(OPColumns::kodeKecamatan, $kodeKecamatan)
             ->where(OPColumns::kodeKelurahan, $kodeKelurahan)
-            ->where(OPColumns::kodeBlok, $kodeBlok);
+            ->where(OPColumns::kodeBlok, $kodeBlok)->orderBy('kd_znt');
         $data = $kode ? $data->where('kd_znt', $kode) : $data;
         throw_if($data->get()->isEmpty(), new ModelNotFoundException());
         return $data->count() > 1 ? $data->get()->mapInto(BlokZonaNilaiTanahDto::class) : new BlokZonaNilaiTanahDto($data->firstOrFail());
