ac8219ee00f1e07b92608ce0454450391d85f808
Abdul Aziz Nurfarhana <abaz.nurfarhana@gmail.com>
2021-10-22 10:42:35 +0700

    Add order by kodeKecamatan ascending

--- src/Services/KecamatanServiceImpl.php
+++ src/Services/KecamatanServiceImpl.php
@@ -6,6 +6,7 @@ namespace Mdigi\PBB\Services;
 
 use Mdigi\PBB\Contracts\KecamatanService as KecamatanContract;
 use Mdigi\PBB\Dtos\Kecamatan as KecamatanDto;
+use Mdigi\PBB\Helpers\OPColumns;
 use Mdigi\PBB\Models\Kecamatan;
 
 class KecamatanServiceImpl implements KecamatanContract
@@ -13,14 +14,19 @@ class KecamatanServiceImpl implements KecamatanContract
 
     public function findAll()
     {
-        return Kecamatan::all()->mapInto(KecamatanDto::class);
+        return $this->getQuery()->get()->mapInto(KecamatanDto::class);
     }
 
     public function findByKecamatan(KecamatanDto $kecamatan)
     {
-        $data = Kecamatan::query();
+        $data = $this->getQuery();
         $data = $kecamatan->kode ? $data->where('kd_kecamatan', $kecamatan->kode) : $data;
         $data = $kecamatan->nama ? $data->where('nm_kecamatan', $kecamatan->nama) : $data;
         return $kecamatan->kode ? new KecamatanDto($data->firstOrFail()) : $data->get()->mapInto(KecamatanDto::class);
+    }
+
+    private function getQuery()
+    {
+        return Kecamatan::query()->orderBy(OPColumns::kodeKecamatan);
     }
 }
